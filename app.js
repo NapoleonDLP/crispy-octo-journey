4833758126d5181591fbd518731394228649d77f
NapoleonDLP <napoleon.dlp@gmail.com>
2021-04-13 05:25:13 -0700

    Added routes for CRUD

--- app.js
+++ app.js
@@ -16,3 +16,13 @@ app.get('/', (request, response) => {
     info: 'Node.js, Express, and Postgres API'
   });
 });
+
+app.get('/users', db.getUsers);
+app.get('/users/:id', db.getUserById);
+app.post('/users', db.createUser);
+app.put('/users/:id', db.updateUser);
+app.delete('/users/:id', db.deleteUser);
+
+app.listen(3000, function () {
+  console.log("Express server listening on port 3000");
+});
