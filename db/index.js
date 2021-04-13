dcf010e5593968067c4e4578edec19d6dd91228c
NapoleonDLP <napoleon.dlp@gmail.com>
2021-04-13 03:34:49 -0700

    Created connection to DB

---
+++ db/index.js
@@ -0,0 +1,10 @@
+var pgp = require('pg-promise');
+var db = pgp('postgres://username:password@host:port/database');
+
+db.one('SELECT $1 AS value', 123)
+  .then((data) => {
+    console.log('DATA:', data.value)
+  })
+  .catch((error) => {
+    console.log('ERROR:', error)
+  });
