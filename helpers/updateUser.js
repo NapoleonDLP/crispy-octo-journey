752f0762c91cf754d8883e57eef13211db98ab59
NapoleonDLP <napoleon.dlp@gmail.com>
2021-04-20 17:19:10 -0700

    Created helper to configure query for user updates

---
+++ helpers/updateUser.js
@@ -0,0 +1,31 @@
+const updateUserQuery = function(body, id) {
+  let storage = {
+    query: 'UPDATE users SET ',
+    entries: []
+  };
+
+  let entryCount = 0;
+
+  for (const entry in body) {
+    if (body[entry]) {
+      entryCount++;
+      storage.entries.push(body[entry]);
+      storage.query += `${entry} = $${entryCount}, `
+    }
+  }
+
+  const to_timestamp = new Date;
+  entryCount++;
+  storage.query += `last_updated = $${entryCount} `;
+  storage.entries.push(to_timestamp);
+
+
+  storage.query += `WHERE id = $${entryCount + 1}`;
+  storage.entries.push(id);
+
+  return storage;
+};
+
+module.exports = {
+  updateUserQuery
+}
