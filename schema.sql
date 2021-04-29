b6e6841c88b222f4ad89131b4113b7c8fa26689a
NapoleonDLP <napoleon.dlp@gmail.com>
2021-04-29 15:57:15 -0700

    Added a check to columns to ensure there are no empty strings

--- schema.sql
+++ schema.sql
@@ -7,10 +7,15 @@ CREATE DATABASE charles_hustle;
 CREATE TABLE users (
   id SERIAL PRIMARY KEY,
   first_name VARCHAR (26) NOT NULL,
+    CHECK (first_name <> ''),
   last_name VARCHAR (26) NOT NULL,
+    CHECK (last_name <> ''),
   email VARCHAR (50) NOT NULL UNIQUE,
+    CHECK (email <> ''),
   slug VARCHAR (50) UNIQUE,
+    CHECK(slug <> ''),
   created TIMESTAMP NOT NULL,
   last_updated TIMESTAMP NOT NULL,
-  pass_hash VARCHAR NOT NULL
+  pass_hash VARCHAR NOT NULL,
+    CHECK (pass_hash <> '')
 );
